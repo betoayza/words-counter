5a0c77ce7d11cfbc60255a0128d8577ff62a3f6c
betoayza <aaayza@gmail.com>
2023-03-16 18:28:38 -0300

    Handler Exceptions added

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 words = input("-------\nWelcome to Words Counter!\nPlease, enter a phrase [-1 to exit]: ")
 
-
 while words != "-1":
-    counter = len(words.split()) 
-    print("The phrase has:", counter, "words!")
+    try:
+        int(words)        
+        words = input("Not number allowed, try again... [-1 to exit]: ")
 
-    words = input("Very good, try other phrase [-1 to exit]: ")
+    except ValueError:
 
+            counter = len(words.split()) 
+            print("The phrase has:", counter, "words!")
+
+            words = input("Very good, try other phrase [-1 to exit]: ")
 
 print("Thanks, come back later!")
